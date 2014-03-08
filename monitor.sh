2ff2ab7d345967df5806de3ed03ec9cf5944e056
Kev++ <vimagick@gmail.com>
2014-03-08 09:59:39 -0700

    update

--- monitor.sh
+++ monitor.sh
@@ -5,13 +5,16 @@ pout=1
 dir=output
 mkdir -p $dir
 
+gpio mode $pin input
+gpio mode $pout output
+
 while gpio wfi $pin rising
 do
     gpio write $pout 1
     jpg=$(date +%s).jpg
     msg="[$(date +%FT%T)] $jpg"
     echo "$msg"
-    raspistill -o $dir/$jpg
+    raspistill -t 1 -q 1 -o $dir/$jpg
     git add $dir
     git commit -m "$msg"
     git push

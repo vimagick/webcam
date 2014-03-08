5769059614eed7a14fdad9898688440f80a736e8
Kev++ <vimagick@gmail.com>
2014-03-08 10:57:55 -0700

    update

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pass=${1:?password is empty}
 pin=0
 pout=1
 dir=output
@@ -11,13 +12,15 @@ gpio mode $pout output
 while gpio wfi $pin rising
 do
     gpio write $pout 1
-    jpg=$(date +%s).jpg
+    jpg=$dir/$(date +%s).jpg
     msg="[$(date +%FT%T)] $jpg"
     echo "$msg"
-    raspistill -t 1 -q 1 -o $dir/$jpg
+    raspistill -t 1 -q 10 -w 800 -h 600 -o $jpg
+    openssl des3 -salt -in $jpg -out $jpg.enc -pass "pass:$pass"
+    rm -f $jpg
     git add $dir
-    git commit -m "$msg"
-    git push
+    git commit -qm "$msg"
+    git push -q
     gpio write $pout 0
     sleep 10
 done
